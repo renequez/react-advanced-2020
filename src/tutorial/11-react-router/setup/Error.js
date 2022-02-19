4623e5869722e20b8759e2898dc09ce46232c82f
renequez <ecristianbujor@gmail.com>
2022-02-19 19:24:20 +0200

    complete part 11

--- src/tutorial/11-react-router/setup/Error.js
+++ src/tutorial/11-react-router/setup/Error.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 const Error = () => {
-  return (
-    <div>
-      <h1>Error Page</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Error Page</h1>
+			<Link to='/' className='btn'>
+				Go back home
+			</Link>
+		</div>
+	);
 };
 
 export default Error;
--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { data } from '../../../data';
 import { Link, useParams } from 'react-router-dom';
+
 const Person = () => {
-  return (
-    <div>
-      <h2>person</h2>
-    </div>
-  );
+	const [name, setName] = useState('');
+	const { id } = useParams();
+
+	useEffect(() => {
+		const newPerson = data.find((person) => person.id === parseInt(id));
+		setName(newPerson.name);
+	}, []);
+
+	return (
+		<div>
+			<h1>{name}</h1>
+			<Link to='/people' className='btn'>
+				Go back
+			</Link>
+		</div>
+	);
 };
 
 export default Person;
