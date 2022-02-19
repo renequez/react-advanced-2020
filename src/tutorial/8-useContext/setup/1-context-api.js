d299eaaef8193b93e4798abd75d37001afaf324c
renequez <ecristianbujor@gmail.com>
2022-02-19 19:23:44 +0200

    complete part 8

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -1,46 +1,44 @@
 import React, { useState, useContext } from 'react';
 import { data } from '../../../data';
-// more components
-// fix - context api, redux (for more complex cases)
+
+const PersonContext = React.createContext(); // creates two components - 1. Provider, 2. Consumer
 
 const ContextAPI = () => {
-  const [people, setPeople] = useState(data);
-  const removePerson = (id) => {
-    setPeople((people) => {
-      return people.filter((person) => person.id !== id);
-    });
-  };
-  return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
-  );
+	const [people, setPeople] = useState(data);
+	const removePerson = (id) => {
+		setPeople((people) => {
+			return people.filter((person) => person.id !== id);
+		});
+	};
+	return (
+		<PersonContext.Provider value={{ people, removePerson }}>
+			<h3>prop drilling</h3>
+			<List />
+		</PersonContext.Provider>
+	);
 };
 
-const List = ({ people, removePerson }) => {
-  return (
-    <>
-      {people.map((person) => {
-        return (
-          <SinglePerson
-            key={person.id}
-            {...person}
-            removePerson={removePerson}
-          />
-        );
-      })}
-    </>
-  );
+const List = () => {
+	const data = useContext(PersonContext);
+
+	return (
+		<>
+			{data.people.map((person) => {
+				return <SinglePerson key={person.id} {...person} />;
+			})}
+		</>
+	);
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
-  return (
-    <div className='item'>
-      <h4>{name}</h4>
-      <button onClick={() => removePerson(id)}>remove</button>
-    </div>
-  );
+const SinglePerson = ({ id, name }) => {
+	const { removePerson } = useContext(PersonContext);
+
+	return (
+		<div className='item'>
+			<h4>{name}</h4>
+			<button onClick={() => removePerson(id)}>remove</button>
+		</div>
+	);
 };
 
 export default ContextAPI;
